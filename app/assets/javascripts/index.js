b58698d6a6a29d40da38c8e9a420e7328ed0eb34
GeoffedUP <35282584+GeoffedUP@users.noreply.github.com>
2019-09-17 21:27:14 -0700

    worked on design

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,38 +1,25 @@
-const Eth = require('./template/ethjs');
-const web3 = require('./template/web3');
+const Eth = require("./template/ethjs");
 
-const $ = require('jquery');
-const Vue = require('vue');
-// const Popper = require('./template/popper');
-
+const $ = require("jquery");
+const Vue = require("vue");
+const Popper = require("./template/popper.min");
 
 // const bootstrap = require('./template/bootstrap.min');
-const main = require('./template/main');
+const main = require("./template/main");
 
-
-
-const mineable_token = require('./template/mineable-token-vue.js');
-
-// import Fundraiser from './template/fundraiser'
-
+// const mineable_token = require('./template/mineable-token-vue.js');
 //import TokenStats from './template/tokenStats'
-
 
 // let fundraiser = new Fundraiser(Eth);
 //let tokenStats = new TokenStats();
-import {
-    bro
-} from './template/bro';
 
+import { bro } from "./template/bro";
 
-$(document).ready(function () {
-    console.log("DOMready");
+$(document).ready(function() {
+  console.log("DOMready");
 
-    console.log(bro('iM THE SHUFFLE MONSTER'))
-    //   fundraiser.init()
+  console.log(bro("iM THE SHUFFLE MONSTER"));
+  //   fundraiser.init()
 
-
-
-    //  tokenStats.init();
-
+  //  tokenStats.init();
 });
--- app/assets/javascripts/template/main.js
+++ app/assets/javascripts/template/main.js
@@ -3,7 +3,17 @@ const Eth = require('./ethjs');
 
 const jQuery = $ = require('jquery');
 
+// $(function () {
+//     var hash = window.location.hash;
+//     hash && $('ul.nav a[href="' + hash + '"]').tab('show');
 
+//     $('.nav-tabs a').click(function (e) {
+//         $(this).tab('show');
+//         var scrollmem = $('body').scrollTop();
+//         window.location.hash = this.hash;
+//         $('html,body').scrollTop(scrollmem);
+//     });
+// });
 
 $(function ($) {
     "use strict";
@@ -44,11 +54,7 @@ $(function ($) {
     });
 
 
-    //scrollspy menu
-    $('body').scrollspy({
-        target: '#navbarSupportedContent',
-        offset: 80
-    });
+
 
     /* offset ends */
 
